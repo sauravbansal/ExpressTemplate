3cad6ab73d66b2f64ea828b4fc2873392163de5c
Saurav Bansal <saurav.bansal@gulaq.com>
2020-06-01 13:09:36 +0530

    response handler changes

---
+++ constants/response-codes.js
@@ -0,0 +1,11 @@
+
+const respCode = {
+  SUCCESS: 'Success',
+  BAD_REQUEST: 'Bad Request',
+  UNAUTHORIZED: 'Unauthorized',
+  NOT_FOUND: 'Resource not found',
+  SERVER_ERROR: 'Internal server error',
+  CODE_NOT_FOUND: 'Unknown code'
+}
+
+module.exports = respCode;
--- utils/response.js
+++ utils/response.js
@@ -3,66 +3,46 @@
  */
 
 const debug = require('./logger').debugger(__filename);
-
+const lodash = require('lodash');
+const constants = require(`${__dirname}/../constants/response-codes`);
 /**
  * Code mapping to response Status/Message/HTTP Code.
  */
 
 const resCodes = {
+  /* to be used for successful get requests */
   200: {
     httpStatus: 200,
-    msg: {
-      status: true,
-      code: 200,
-      message: 'Processed Successfully'
+    body: {
+      code: 'SUCCESS'
     }
   },
-  201: {
-    httpStatus: 201,
-    msg: {
-      status: true,
-      code: 201,
-      message: 'Created/Modified Successfully'
-    }
-  },
-  202: {
-    httpStatus: 202,
-    msg: {
-      status: true,
-      code: 202,
-      message: 'Deleted Successfully'
-    }
-  },
+  /* to be used for bad request or missing parameters in the request */
   400: {
     httpStatus: 400,
-    msg: {
-      status: false,
-      code: 400,
-      message: 'Incomplete/Bad Request'
+    body: {
+      code: 'BAD_REQUEST'
     }
   },
+  /* Not to be used in api response - system default use */
   401: {
     httpStatus: 401,
-    msg: {
-      status: false,
-      code: 401,
-      message: 'Unauthorized Request'
+    body: {
+      code: 'UNAUTHORIZED'
     }
   },
+  /* Not to be used in api response - system default use */
   404: {
     httpStatus: 404,
-    msg: {
-      status: false,
-      code: 404,
-      message: 'Requested Resource Not Found'
+    body: {
+      code: 'NOT_FOUND'
     }
   },
+  /* Not to be used in api response - system default uses */
   500: {
     httpStatus: 500,
-    msg: {
-      status: false,
-      code: 500,
-      message: 'Internal Server Error'
+    body: {
+      code: 'SERVER_ERROR'
     }
   }
 };
@@ -73,27 +53,44 @@ const resCodes = {
 
 module.exports = function (err, res, resCode, resData) {
   debug(err, resCode, resData);
-  var response = resCodes[resCode];
-  if (!response) {
-    response = resCodes[500];
+  let response = {};
+  if (resCodes[resCode] !== undefined && !err) {
+    response = JSON.parse(JSON.stringify(resCodes[resCode]));
+  } else {
+    response = JSON.parse(JSON.stringify(resCodes[500]));
   }
 
   if (err) {
-    response.msg.error = {};
+    response.body.error = {};
 
     if (err.name) {
-      response.msg.error.name = err.name;
+      response.body.error.name = err.name;
     }
 
     if (err.message) {
-      response.msg.error.message = err.message;
+      response.body.error.message = err.message;
     }
   }
 
   if (resData) {
-    response.msg.result = resData;
+    if (typeof resData === 'string') {
+      response.body.code = resData;
+    } else {
+      if (resData.code !== undefined) {
+        response.body.code = resData.code;
+        delete resData.code;
+      }
+
+      if (!lodash.isEmpty(resData)) {
+        response.body.data = resData;
+      }
+    }
+
+    if (resData.code !== undefined && !constants[response.body.code]) {
+      response.body.code = 'CODE_NOT_FOUND';
+    }
   }
 
   debug(response);
-  res.status(response.httpStatus).send(response.msg);
+  res.status(response.httpStatus).send(response.body);
 };
